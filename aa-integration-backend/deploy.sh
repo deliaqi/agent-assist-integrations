91c79fe0faa1ff0a0e23749a3c27af55d09c82de
Jiaqi Liu <jiaqiliu@google.com>
2025-02-15 10:09:20 -0800

    print out root directory and subdir.

--- aa-integration-backend/deploy.sh
+++ aa-integration-backend/deploy.sh
@@ -45,6 +45,9 @@
 # How to run this script:
 #     cd aa-integration-backend/ && sh ./deploy.sh
 
+pwd
+
+ls
 
 # TODO: Please update the following fields according to your existing resources.
 export GCP_PROJECT_ID=${GCP_PROJECT_ID:'your-project-id'}
